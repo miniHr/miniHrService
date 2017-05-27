bf15970897f52e7171f10387c9a5548c9b6efb14
tangbw <tangbw@allinpay.com>
2017-05-27 14:46:40 +0800

    修改支付类
    Signed-off-by: tangbw <tangbw@allinpay.com>

--- src/main/java/com/miniHr/pay/UnifiedorderPay.java
+++ src/main/java/com/miniHr/pay/UnifiedorderPay.java
@@ -33,9 +33,53 @@ public class UnifiedorderPay {
 	    }
 	    return rand.toString();
 	}
-	public static String WechatPay(String amount,String body,String openid){
+	//统一下单支付
+	public static Map<String, Object> WechatPay(Map<String, String> map){
 		Map<String, Object> resParams = new HashMap<String, Object>();
+		String amount=map.get("amount");
+		String body="展位购买-展位"+map.get("boothid");
+		String openid=map.get("openid");
+		SortedMap<String, String> parameters =getReqParameters(amount,body,openid);
+        String xmlInfo = StringUtil.parseXML(parameters);
+        System.out.println("reqMsg===="+xmlInfo);
+        
+        Map<String, Object> params = new HashMap<String, Object>();
+		params.put("reqMsg", xmlInfo);
+        String respXml=HttpHelper.post(reqUrl, params);
 		
+		System.out.println("respXml===="+respXml);
+		resParams=getRespState(respXml);
+		respXml = StringUtil.deleteCdata(respXml);
+		if(resParams.get("resCode").equals("00")){
+			resParams.put("redirectParamsMap", JSON.toString(getH5Map(respXml)));
+		}
+		return resParams;
+		
+	}
+	//获取返回报文的交易状态
+	public static Map<String, Object> getRespState(String respXml){
+		Map<String, Object> resParams = new HashMap<String, Object>();
+		respXml = StringUtil.deleteCdata(respXml);
+		String retCode=StringUtil.getTagValue(respXml, "return_code");//通讯状态
+		String retMsg = StringUtil.getTagValue(respXml, "return_msg");
+		if("SUCCESS".equals(retCode) && WechatSecurity.verify(respXml, key)){
+			retCode=StringUtil.getTagValue(respXml,"result_code");//业务状态
+			if("SUCCESS".equals(retCode)){
+				retCode="00";
+			}else{
+				retCode="01";
+				retMsg=StringUtil.getTagValue(respXml, "err_code_des");
+			}
+		}else{
+			retCode="01";
+		}
+		resParams.put("resCode", retCode);
+		resParams.put("resMsg",retMsg);
+		return resParams;
+	}
+	//组装请求参数
+	private static SortedMap<String, String> getReqParameters(String amount,
+			String body, String openid) {
 		SortedMap<String, String> parameters = new TreeMap<String, String>();  
         parameters.put("appid", appid);  
         parameters.put("mch_id", mchtId);  
@@ -57,58 +101,35 @@ public class UnifiedorderPay {
         parameters.put("product_id", "");  
         parameters.put("limit_pay", "no_credit"); //no_credit 
         parameters.put("openid", openid);  
-        parameters.put("sign", WechatSecurity.sign(parameters,key));  
-        
-        String xmlInfo = StringUtil.parseXML(parameters);
-        System.out.println(xmlInfo);
-        
-        Map<String, Object> params = new HashMap<String, Object>();
-		params.put("reqMsg", xmlInfo);
-        String respXml=HttpHelper.post(reqUrl, params);
-		
-		System.out.println(respXml);
-		respXml = StringUtil.deleteCdata(respXml);
-		String retCode=StringUtil.getTagValue(respXml, "return_code");//通讯状态
-		String retMsg = StringUtil.getTagValue(respXml, "return_msg");
-		if("SUCCESS".equals(retCode) && WechatSecurity.verify(respXml, key)){
-			retCode=StringUtil.getTagValue(respXml,"result_code");//业务状态
-			if("SUCCESS".equals(retCode)){
-				SimpleDateFormat sdf = new SimpleDateFormat(DateUtil.DF_YMDHMS);
-				long millionSeconds = 0;
-				try {
-					millionSeconds = sdf.parse(DateUtil.formatCurrDateTime(DateUtil.DF_YMDHMS)).getTime();
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}//毫秒
-				SortedMap<String, String> redirectParamsMap= new TreeMap<String, String>();
-				redirectParamsMap.put("appId", appid);
-				redirectParamsMap.put("timeStamp", String.valueOf(millionSeconds).substring(0,10));
-				redirectParamsMap.put("nonceStr", getRandStr());
-				redirectParamsMap.put("package", "prepay_id="+StringUtil.getTagValue(respXml,"prepay_id"));
-				redirectParamsMap.put("signType", "MD5");
-				redirectParamsMap.put("paySign",WechatSecurity.sign(redirectParamsMap,key));
-				System.out.println(redirectParamsMap.toString());
-				resParams.put("redirectParamsMap", JSON.toString(redirectParamsMap));
-				retCode="00";
-			}else{
-				retCode="01";
-				retMsg=StringUtil.getTagValue(respXml, "err_code_des");
-			}
-		}else{
-			retCode="01";
-		}
-		resParams.put("resCode", retCode);
-		resParams.put("resMsg",retMsg);
-		return JSON.toString(resParams);
-		
+        parameters.put("sign", WechatSecurity.sign(parameters,key)); 
+		return parameters;
+	}
+	//组装H5控件请求参数
+	private static SortedMap<String, String> getH5Map(String respXml) {
+		SimpleDateFormat sdf = new SimpleDateFormat(DateUtil.DF_YMDHMS);
+		long millionSeconds = 0;
+		try {
+			millionSeconds = sdf.parse(DateUtil.formatCurrDateTime(DateUtil.DF_YMDHMS)).getTime();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//毫秒
+		SortedMap<String, String> redirectParamsMap= new TreeMap<String, String>();
+		redirectParamsMap.put("appId", appid);
+		redirectParamsMap.put("timeStamp", String.valueOf(millionSeconds).substring(0,10));
+		redirectParamsMap.put("nonceStr", getRandStr());
+		redirectParamsMap.put("package", "prepay_id="+StringUtil.getTagValue(respXml,"prepay_id"));
+		redirectParamsMap.put("signType", "MD5");
+		redirectParamsMap.put("paySign",WechatSecurity.sign(redirectParamsMap,key));
+		return redirectParamsMap;
 	}
 	
 	public static void main(String[] args) {
-		String amount="1";
-		String body="测试";
-		String openid="oCeMZ0WUojqOrmtZOA0f1-8qvJg0";
-		System.out.println(WechatPay(amount,body,openid));
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("amount", "1");
+		map.put("boothid", "15");
+		map.put("openid", "oCeMZ0WUojqOrmtZOA0f1-8qvJg0");
+		System.out.println(WechatPay(map));
 		
 	}
 }
