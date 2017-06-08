476e56ea845e7e271e5b3199b5a393b2cc1476ba
yinkun <yinkun@allinpay.com>
2017-06-08 12:18:33 +0800

    修改sql脚本

--- doc/sql/company_info.sql
+++ doc/sql/company_info.sql
@@ -26,12 +26,12 @@ CREATE TABLE `COMPANY_INFO` (
   `SCALE` varchar(16) default '' COMMENT '公司规模',
   `ADDRESS` varchar(64) default '' COMMENT '公司地址',
   `WELFARE` varchar(64) default '' COMMENT '公司福利',
-  `NAME` varchar(16) NOT NULL default '' COMMENT '联系人',
+  `NAME` varchar(16) default '' COMMENT '联系人',
   `PHONE` varchar(16) NOT NULL default '' COMMENT '联系电话',
-  `BOOTH_ID` int(8) NOT NULL COMMENT '展位ID',
+  `BOOTH_ID` int(8) COMMENT '展位ID',
   `CREATE_DT` timestamp NULL default CURRENT_TIMESTAMP COMMENT '创建时间',
   `CREATER` varchar(19) default '' COMMENT '创建人',
-  `UPDATE_DT` timestamp NULL default NULL on update CURRENT_TIMESTAMP COMMENT '更新时间',
+  `UPDATE_DT` timestamp COMMENT '更新时间',
   `UPDATER` varchar(19) default '' COMMENT '更新人',
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
