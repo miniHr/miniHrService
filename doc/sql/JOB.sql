04290c4befdf2373fd7e2bcdecd086993d560469
xugr <xugr>
2017-06-15 23:35:19 +0800

    job.sql脚本更改

--- doc/sql/JOB.sql
+++ doc/sql/JOB.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50520
 File Encoding         : 65001
 
-Date: 2017-06-13 16:17:39
+Date: 2017-06-15 23:34:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,10 +26,11 @@ CREATE TABLE `JOB` (
   `JOB_DETAIL` varchar(128) NOT NULL COMMENT '职位详情',
   `JOB_SALARY` varchar(16) NOT NULL,
   `GENDER` char(1) DEFAULT NULL COMMENT '性别要求',
-  `AGE` varchar(16) DEFAULT NULL COMMENT '年龄要求',
+  `MINAGE` varchar(16) DEFAULT NULL COMMENT '最小年龄要求',
+  `MAXAGE` varchar(16) DEFAULT NULL COMMENT '最大年龄要求',
   `INDUSTRY` varchar(16) DEFAULT NULL COMMENT '行业要求',
   `WORKTIME` int(4) DEFAULT NULL COMMENT '从业年龄要求',
-  `EDUCATION` varchar(16) DEFAULT NULL COMMENT '学历要求',
+  `EDUCATION` varchar(16) CHARACTER SET utf8 DEFAULT NULL COMMENT '学历要求',
   `MAJOR` varchar(32) DEFAULT NULL COMMENT '专业要求',
   `CREATE_DT` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   `CREATER` varchar(19) DEFAULT NULL COMMENT '创建人',
