17fb94ca569c49ae000f30c17cae32d2ebd12402
yinkun <yinkun@allinpay.com>
2017-07-27 09:32:44 +0800

    更改测试案例

--- src/test/java/test/miniHr/data/ReadFileTest.java
+++ src/test/java/test/miniHr/data/ReadFileTest.java
@@ -13,13 +13,58 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class ReadFileTest {
     public static void main(String[] args) {
         try {
-            readAndDoData("D:/data.xlsx");
+            jobData("D:/data.xlsx");
+            companyData("D:/data.xlsx");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void readAndDoData(String fileName) throws Exception {
+    private static void companyData(String fileName) throws Exception {
+        FileInputStream bean = new FileInputStream(fileName);
+        Workbook workbook = WorkbookFactory.create(bean);
+        StringBuilder sb = new StringBuilder();
+        Sheet sheet = workbook.getSheetAt(1);
+        Row row = null;
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            StringBuilder sb2 = new StringBuilder(
+                "insert into COMPANY_INFO(id,booth_id,COMPANY_NAME,information,SCALE,ADDRESS,WELFARE,NAME,PHONE,IMAGE) values(");
+            if ((row = sheet.getRow(i)) != null) {
+                for (int j = 0; j < 10; j++) {
+                    String value = null;
+                    Cell cell = row.getCell(j);
+                    if (cell == null || Cell.CELL_TYPE_BLANK == cell.getCellType()) {
+                        value = "";
+                    } else {
+                        if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                            HSSFDataFormatter dataFormatter = new HSSFDataFormatter();
+                            value = dataFormatter.formatCellValue(cell);
+                        } else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+                            value = String.valueOf(cell.getStringCellValue());
+                        }
+                    }
+                    value = value.replace("\n", "");
+                    value = value.replaceAll("\\s*", "");
+                    if (j == 0) {
+                        sb2.append(value).append(",");
+                    } else if (j == 1) {
+                        sb2.append(value).append(",");
+                    } else {
+                        sb2.append("'").append(value).append("'").append(",");
+                    }
+                }
+                sb2.setLength(sb2.length() - 1);
+                sb2.append(");").append(System.getProperty("line.separator"));
+                sb.append(sb2);
+            }
+        }
+        FileWriter writer = new FileWriter("D:/company_info.sql");
+        writer.write(sb.toString());
+        writer.close();
+        bean.close();
+    }
+
+    public static void jobData(String fileName) throws Exception {
         FileInputStream bean = new FileInputStream(fileName);
         Workbook workbook = WorkbookFactory.create(bean);
         StringBuilder sb = new StringBuilder();
@@ -57,7 +102,7 @@ public class ReadFileTest {
                 sb.append(sb2);
             }
         }
-        FileWriter writer=new FileWriter("D:/data.sql");
+        FileWriter writer = new FileWriter("D:/job.sql");
         writer.write(sb.toString());
         writer.close();
         bean.close();
